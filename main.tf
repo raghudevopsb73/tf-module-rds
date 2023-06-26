0d348ba00c9effc2ff9fac60b6a3878749e9d33c
Raghu K <raghuk.vit@gmail.com>
2023-06-26 07:57:46 +0530

    Update tf-module-rds/

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
   vpc_id      = var.vpc_id
@@ -38,7 +38,7 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   storage_encrypted      = true
   kms_key_id             = var.kms_key_arn
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
 resource "aws_rds_cluster_instance" "main" {
